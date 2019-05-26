2fda4be3fbd8cab76dc8e7705502b3a87e0c1910
MiriamRG13 <50623025+MiriamRG13@users.noreply.github.com>
2019-05-26 13:57:35 +0200

    Update Resize.h

--- Resize.h
+++ Resize.h
@@ -1,8 +1,24 @@
 #ifndef PROJECT_RESIZE_H
 #define PROJECT_RESIZE_H
 
+/*********************************************************
+  Function: Resize
+		Called source code to resize an image
+	Input parameters:
+		Mat img - Contains the image to resize. By value.
+	Output parameters:
+		void
+**********************************************************/
 void Resize(cv::Mat img);
 
+/***************************************************************
+  Function: resizing
+		This function is called when there are a trackbar event
+	Input parameters:
+		int, void* - Presets the parameters to zero.
+	Output parameters:
+		void
+***************************************************************/
 void resizing(int, void*);
 
 #endif
