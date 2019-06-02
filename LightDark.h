72cb6aaa1a843878b2c4599182bc4849e756ccb7
iAmTonyos <32823995+iAmTonyos@users.noreply.github.com>
2019-06-03 00:31:41 +0200

    Create LightDark.h
    goes with LightDark.cpp

---
+++ LightDark.h
@@ -0,0 +1,24 @@
+#ifndef PROJECT_LIGHTDARK_H
+#define PROJECT_LIGHTDARK_H
+
+/*********************************************************
+  Function: rebright
+		Called source code to change brightness of an image
+	Input parameters:
+		Mat img - Contains the image to rebright. By value.
+	Output parameters:
+		void
+**********************************************************/
+void changebright(cv::Mat img);
+
+/***************************************************************
+  Function: changebright
+		This function is called when there are a trackbar event
+	Input parameters:
+		int, void* - Presets the parameters to zero.
+	Output parameters:
+		void
+***************************************************************/
+void rebright(int, void*);
+
+#endif
